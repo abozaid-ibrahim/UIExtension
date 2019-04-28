16148cfba18b604a21da91236135e246b5e3d2bf
abuzeid ibrahim <abuzeid7@gmail.com>
2019-04-28 12:43:13 +0200

    Initial Commit

---
+++ UIExtensions/UIExtensions.h
@@ -0,0 +1,19 @@
+//
+//  UIExtensions.h
+//  UIExtensions
+//
+//  Created by abuzeid on 4/28/19.
+//  Copyright © 2019 abuzeid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIExtensions.
+FOUNDATION_EXPORT double UIExtensionsVersionNumber;
+
+//! Project version string for UIExtensions.
+FOUNDATION_EXPORT const unsigned char UIExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIExtensions/PublicHeader.h>
+
+
